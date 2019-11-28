23614cd4df6e2d65fd267e667a692d8b44a23fcc
Yakov <kasyanov_yv@bk.ru>
2019-11-28 11:56:33 +0600

    mechanics first step

---
+++ Mechanics/Main_Tests.py
@@ -0,0 +1,12 @@
+from Dice_Roll import *
+from math import *
+
+def do_maint_test(skill_val, difficult_modifier=0):
+    d20_roll_result = d20_roll()
+    return [skill_val + difficult_modifier - d20_roll_result, d20_roll_result]
+
+def do_opposed_test(skill_val_1, skill_val_2, difficult_modifier_1=0, difficult_modifier_2=0):
+    result_1 = do_maint_test(skill_val_1, difficult_modifier_1)
+    result_2 = do_maint_test(skill_val_2, difficult_modifier_2)
+    return [result_1 - result_2, result_1, result_2]
+
