a505f957646153fa15953c0d3e546446d32e221d
Yakov <kasyanov_yv@bk.ru>
2019-11-28 15:13:08 +0600

    mechanics 2 step

--- Mechanics/Main_Tests.py
+++ Mechanics/Main_Tests.py
@@ -1,12 +1,26 @@
 from Dice_Roll import *
 from math import *
 
-def do_maint_test(skill_val, difficult_modifier=0):
+
+def do_main_test(skill_val, difficult_modifier=0):
     d20_roll_result = d20_roll()
-    return [skill_val + difficult_modifier - d20_roll_result, d20_roll_result]
+    delta = skill_val + difficult_modifier - d20_roll_result
+    return [delta, d20_roll_result]
+
 
 def do_opposed_test(skill_val_1, skill_val_2, difficult_modifier_1=0, difficult_modifier_2=0):
-    result_1 = do_maint_test(skill_val_1, difficult_modifier_1)
-    result_2 = do_maint_test(skill_val_2, difficult_modifier_2)
-    return [result_1 - result_2, result_1, result_2]
+    result_1 = do_main_test(skill_val_1, difficult_modifier_1)
+    result_2 = do_main_test(skill_val_2, difficult_modifier_2)
+    opposed_delta = result_1[0] - result_2[0]
+    return [opposed_delta, result_1[0], result_1[1], result_2[0], result_2[1]]
+
+def do_hidden_attack_test(attack_skill_val_A, perception_val_D, difficult_modifier_A=0, difficult_modifier_D=0):
+    if do_main_test(perception_val_D, difficult_modifier_D) >= 0:
+        return 'attack was detected'
+    else:
+        return do_main_test(attack_skill_val_A, difficult_modifier_A)
+
+
+def do_attack_opposed_test(skill_val_1, skill_val_2, difficult_modifier_1=0, difficult_modifier_2=0):
+    result = do_opposed_test(skill_val_1, skill_val_2, difficult_modifier_1, difficult_modifier_2)
 
